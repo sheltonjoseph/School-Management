1250914232dcd05fdde56be4268f36f0fa30b8ac
sheltonjoseph <shelton013@gmail.com>
2022-10-05 18:15:23 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Home } from './Pages/Home';
+import StudentsList from './Pages/StudentsList.jsx';
 
 function App() {
   return (
     <div className="App">
-<Home/>
+{/* <Home/> */}
+<StudentsList/>
     </div>
   );
 }
---
+++ src/Pages/StudentsList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  NavBar from '../Components/navBar'
+
+
+const StudentsList = () => {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  )
+}
+
+export default StudentsList
